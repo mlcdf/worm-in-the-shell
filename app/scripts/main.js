0a32b73c60bd99f10d57b74c0a1dab092d772b1c
Maxime Le Conte des Floris <maxime.lcdf@gmail.com>
2016-12-15 15:59:49 +0100

    Add navigation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+const actionButtons = $('button[go]')
+const sections = $('.section')
+
+// Hide all the section exect the first one.
+sections.slice(1).hide()
+
+actionButtons.each(function () {
+  // btn stores the current button
+  const btn = $(this)
+
+  // When a button is clicked, do
+  $(this).click(() => {
+
+    // Hide the current section
+    $(this).parents('.section').hide()
+
+    // Show the section pointed by the button attribut 'go'
+    $('#' + btn.attr('go')).show()
+  })
+})
